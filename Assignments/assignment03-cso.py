dcd5d6d6bbb700f04754b8418205fffda5957bf1
Jamie <jamietohall@hotmail.com>
2023-10-23 14:25:59 +0100

    Added CSO assignment

---
+++ Assignments/assignment03-cso.py
@@ -0,0 +1,20 @@
+# Data Representation - Assignment 3
+# Author: Jamie Tohall
+
+# Write a program that retrieves the dataset for the "exchequer account (historical series)" 
+# from the CSO, and stores it into a file called "cso.json"
+
+import requests # Import relevant modules
+import json
+
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
+# url for the exchequer account (historical series) dataset, JSON format
+
+def getalldata(): 
+    response = requests.get(url)
+    return response.json()
+
+if __name__ == "__main__":
+    #print(getalldata())
+    with open("cso.json", "w") as f:
+        print(json.dumps(getalldata()), file=f)
